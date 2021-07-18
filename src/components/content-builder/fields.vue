b43bf716508eb30bd3b7a4ca0a3927bc0b04d139
Erik Tran <erik.tran.tt@gmail.com>
2021-07-18 21:31:58 +1000

    Turn component generator to render on <client-only>

--- src/components/content-builder/fields.vue
+++ src/components/content-builder/fields.vue
@@ -1,13 +1,13 @@
 <template>
-  <keep-alive>
-    <component
-      :is="field"
-      :data="data"
-      v-bind="$attrs"
-      :style="$vnode.data['style']"
-      :class="$vnode.data['staticClass']"
-    ></component>
-  </keep-alive>
+  <!--
+  Having the <client-only> tag will pass styles and classes assigned
+  from the parents through to the dynamic component to be rendered.
+  -->
+  <client-only>
+    <keep-alive>
+      <component :is="field" :data="data" />
+    </keep-alive>
+  </client-only>
 </template>
 
 <script lang="ts">
@@ -21,7 +21,8 @@ export default class ContentBuilderFields extends Vue {
   };
 
   get field(): unknown {
-    return () => import(`~/components/content-builder/${this.data.type}-field`);
+    return () =>
+      import(`~/components/content-builder/${this.data.type}-field.vue`);
   }
 }
 </script>
